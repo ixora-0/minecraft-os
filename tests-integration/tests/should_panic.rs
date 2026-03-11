79a8ef755d2849464373d17b6dd5d92c4366148a
ixora-0 <39961970+ixora-0@users.noreply.github.com>
2026-03-10 21:41:06 -0400

    test: Add panic handling test.

---
+++ tests-integration/tests/should_panic.rs
@@ -0,0 +1,32 @@
+#![no_std]
+#![cfg_attr(test, no_main)]
+#![feature(custom_test_frameworks)]
+#![test_runner(tests_integration::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use core::panic::PanicInfo;
+use kernel::{serial_print, serial_println};
+use tests_integration::{QemuExitCode, exit_qemu};
+
+use bootloader_api::{entry_point, info::BootInfo};
+
+entry_point!(test_kernel_main);
+/// Entry point for `cargo test`
+fn test_kernel_main(_boot_info: &'static mut BootInfo) -> ! {
+    should_fail();
+    serial_println!("[test did not panic]");
+    exit_qemu(QemuExitCode::Failed);
+    kernel::hlt_loop()
+}
+
+fn should_fail() {
+    serial_print!("should_panic::should_fail...\t");
+    assert_eq!(0, 1);
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    serial_println!("[ok]");
+    exit_qemu(QemuExitCode::Success);
+    loop {}
+}
